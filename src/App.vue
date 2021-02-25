df3801ce5f082a38715abb2dbbef9f57b14f951b
cdbitmana <cdbitmana@gmail.com>
2021-02-25 09:40:50 +0900

    vue로 toast ui editor 구현

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="editor"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Editor from '@toast-ui/editor';
+import 'codemirror/lib/codemirror.css'; // Editor's Dependency Style
+import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
+
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+       
+  },
+  mounted(){
+    const editor = new Editor({
+    el: document.querySelector('#editor'),
+    height: '500px',
+    initialEditType: 'markdown',
+    previewStyle: 'vertical'
+  });
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
